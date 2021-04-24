b641326d8e5c9185646ac75cc8e33f4539ca6829
Marcin <m.liebiediew@gmail.com>
2021-04-24 21:15:27 +0200

    update fish functions vi/vim and create new function focus

---
+++ .config/fish/functions/focus.fish
@@ -0,0 +1,5 @@
+# Defined interactively
+function focus
+sudo pkill picom
+i3-msg gaps inner all set 0
+end
--- .config/fish/functions/v.fish
+++
@@ -1,9 +0,0 @@
-function v
-	if test (count $argv) -ne 1
-nvim $PWD +':cd $PWD'
-else if test -d $argv[1]
-nvim $argv +':cd ' + $argv
-else if test -f $argv[1]
-nvim $argv +':cd %:h'
-end
-end
